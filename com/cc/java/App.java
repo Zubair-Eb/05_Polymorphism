022f917bbe9fcf43b94c9fdf2da926ff0a5fe6d5
Zubair <zubair.yousaf-eberl@gmx.de>
2022-04-14 09:24:35 +0200

    SuperClass with 3 SubClasses

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,23 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        output(queen.doYourJob());
+        output(worker.doYourJob());
+        output(drone.doYourJob());
+
+        output("------------------");
+
+        output(queen.fly());
+        output(worker.fly());
+        output(drone.fly());
+
+
+
     }
 
     public static void output(String outputStr) {
